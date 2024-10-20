6138bd37244b825a294d4b6b65bd3aa287b7f79d
Prakash Kumar <prakashkumar01031985@gmail.com>
2024-10-21 04:08:58 +0530

    Lecture 04 first part

---
+++ Lecture 04 = Patterns/number_square.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+
+    // for (int i=0;i<n;i++){
+    //     for (int j=0;j<n;j++){
+    //         cout<<char(65+j)<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // int num=1;
+    // for(int i=0;i<n;i++){
+    //     for (int j=0; j<n;j++){
+    //         cout<<num<<" ";
+    //         num++;
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    // char ch= 'A';
+    // int num=0;
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<n;j++){
+    //         cout<<ch<<" ";
+    //         ch++;
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+    return 0;
+}
+
+
+// Outer loop => Controls number of lines
+// Inner loop => Controls elements in each lines
